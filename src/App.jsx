bdf772e16c13abb33a11188dc88e75ae9adcb145
marcellena <112984768+marcellena@users.noreply.github.com>
2026-03-03 16:37:50 -0500

    App.jsx again
    Sorry I broke it, but I put it back.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TaskList from "./components/TaskList";
 import Contact from "./components/Contact";
-=======
-import './App.css'
-import * as Env from "./environments";
-import Parse from "parse"
-import Components from './Components/Components'
->>>>>>> 41d5ade (irrelevant)
 
 export default function App() {
   return (
